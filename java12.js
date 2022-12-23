ebd81dc67b08ff5b0593bec1545d4020e3a5af1a
lisuuu96 <120398096+lisuuu96@users.noreply.github.com>
2022-12-23 17:42:46 +0100

    Update java12.js

---
+++ java12.js
@@ -0,0 +1,4 @@
+let num1 = n
+function check(num1) {let n = 150 ; 
+ if (n===100-200){console.log('Liczba znajduje sie w przedziale'); }
+else (n!==100-200); {console.log('Liczba nie znajduje sie w przedziale')}}
